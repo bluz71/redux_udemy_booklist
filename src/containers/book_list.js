1551e8b3b55716aff8402495e7d506f14eccd3ed
Dennis B <bluz71@users.noreply.github.com>
2017-07-03 14:55:06 +1000

    Completed book-list example.

---
+++ src/containers/book_list.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectBook } from "../actions/index";
+import { bindActionCreators } from "redux";
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // The returned object here will end up as this.props in the BookList class
+  // above. The connect call below is the glue that binds Redux and React
+  // together.
+  return {
+    books: state.books // state.books is populated in reducers/index.js
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
